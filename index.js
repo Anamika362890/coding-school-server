f487ad078134fbc3bd8cd28b4bd474b1ac98fd03
unknown <anamika362890@gmail.com>
2022-10-25 16:29:55 +0600

    courseDataEntry

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+const courses = require('./data/courses.json');
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 
 app.get('/', (req, res) => {
     res.send('School Code is running!')
