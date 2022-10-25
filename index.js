954b935c14e41fffc36fe14adfc7cb8b7b8f9236
unknown <anamika362890@gmail.com>
2022-10-25 16:26:39 +0600

    corsAdd

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const cors = require('cors')
+const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('School Code is running!')
